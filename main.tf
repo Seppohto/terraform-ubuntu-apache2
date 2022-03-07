cf4a55a18eb3f99d0b8400af8825177cdf116549
seppohto <olli.uronen@gmail.com>
2022-03-07 11:44:15 +0200

    initialize

---
+++ main.tf
@@ -0,0 +1,60 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  tenant_id = var.tenant_id
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = "westeurope"
+  
+  tags = {
+    Environment = "Ollin kotitoimisto"
+    Team = "OlliOps"
+  }
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "example" {
+  name                  = "content"
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_management_policy" "example" {
+  storage_account_id = azurerm_storage_account.example.id
+
+  rule {
+    name    = "rule1"
+    enabled = true
+    filters {
+      prefix_match = ["content"]
+      blob_types   = ["blockBlob"]
+    }
+    actions {
+      base_blob {
+        delete_after_days_since_modification_greater_than          = 1
+      }
+    }
+  }
+}
+
---
+++ variable.tf
@@ -0,0 +1,10 @@
+variable "subscription_id" {}
+variable "tenant_id" {}
+
+
+variable "resource_group_name" {
+  default = "OllintoinenTFResourceGroup"
+}
+variable "storage_account_name" {
+  default = "ollinexamplesorage01"
+}
